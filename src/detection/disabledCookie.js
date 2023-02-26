08d5f4391c8dba198ffc834d5197f36e801f9d68
shiqiang.sun <shiqiang.sun@tongdun.cn>
2023-02-26 20:19:51 +0800

    add 'disabledCookie' detection logic

--- src/detection/disabledCookie.js
+++ src/detection/disabledCookie.js
@@ -4,7 +4,15 @@ export default function getDisabledCookie() {
     if (navigator.cookieEnabled) {
       status = 0
     } else {
-      status = 1
+      try {
+        document.cookie = "TDJScookieDemo=test; SameSite=Strict;";
+        if (document.cookie.indexOf("TDJScookieDemo=") !== -1) {
+          status = 1
+        }
+        document.cookie = "TDJScookieDemo=test; SameSite=Strict; expires=Thu, 01-Jan-1970 00:00:01 GMT"
+      } catch (e) {
+        status = 1
+      }
     }
   }
   return status
