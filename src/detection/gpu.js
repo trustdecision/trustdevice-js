01dfce71b76ce79247038e7606968ff25b3b576f
shiqiang.sun <shiqiang.sun@tongdun.cn>
2023-03-27 10:11:26 +0800

    add 'loseContext' to optimize performance

--- src/detection/gpu.js
+++ src/detection/gpu.js
@@ -3,7 +3,14 @@ export default function getGpu() {
     const canvas = document.createElement('canvas')
     const gl = canvas.getContext('webgl')
     const debugInfo = gl.getExtension('WEBGL_debug_renderer_info')
-    return `${gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL)}|${gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL)}`
+    const result =  `${gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL)}|${gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL)}`
+    try {
+      gl.getExtension('WEBGL_lose_context')
+        .loseContext() // 清除WEBGL
+    } catch (e) {
+      /* .getExtension may be absent or broken (blocked by extension) */
+    }
+    return result || ''
   } catch (e) {
     return ''
   }
